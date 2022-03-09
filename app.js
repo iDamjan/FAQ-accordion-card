63650e2ab8186cef970759ca49041e56708ec4f3
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-03-09 19:06:08 +0100

    Finishing CSS styles & added event listener for accordion question

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const question = document.querySelector('.accordion_question');
-const answer = document.querySelector('.accordion_collapse'); 
+const accordionQuestionOne = document.querySelector('.accordion_questionOne');
+const accordionQuestionTwo = document.querySelector('.accordion_questionTwo');
+const accordionQuestionThree = document.querySelector('.accordion_questionThree');
+const accordionQuestionFour = document.querySelector('.accordion_questionFour');
+const accordionQuestionFive = document.querySelector('.accordion_questionFive');
+const accordionAnswer = document.querySelector('.accordion_collapse'); 
+
+accordionQuestionOne.addEventListener('click', (e) =>{
+    accordionAnswer.classList.toggle('accordion_show');
+
+})
+
+accordionQuestionTwo.addEventListener('click', (e) =>{
+    accordionAnswer.classList.toggle('accordion_show');
+
+})
+
+accordionQuestionThree.addEventListener('click', (e) =>{
+    accordionAnswer.classList.toggle('accordion_show');
+
+})
+
+accordionQuestionFour.addEventListener('click', (e) =>{
+    accordionAnswer.classList.toggle('accordion_show');
+
+})
+
+accordionQuestionFive.addEventListener('click', (e) =>{
+    accordionAnswer.classList.toggle('accordion_show');
+
+})
 
