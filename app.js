3932e3314df5b589bbf7902cfac8a229546e4c45
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-03-12 15:55:09 +0100

    Implemented functionality that closes all other accordion answers, except clicked question

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const accordionItem = document.querySelectorAll('.accordion_item')
-const arrows = document.querySelectorAll('.arrow');
+const accordionItems = document.querySelectorAll(".accordion_item");
+const arrows = document.querySelectorAll(".arrow");
 
 //targeting each accordion question of the accordion item separately //
 
-accordionItem.forEach(item => {
-    // console.log(item);
-    // console.log(item.nextElementSibling)
-    item.addEventListener('click', (e)=>{
+accordionItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.nextElementSibling.classList.toggle("accordion_show");
+    item.firstElementChild.nextElementSibling.classList.toggle("arrow_rotate");
 
-        item.nextElementSibling.classList.toggle('accordion_show');
-        item.firstElementChild.nextElementSibling.classList.toggle('arrow_rotate');
+    for (i = 0; i < accordionItems.length; i++) {
+      const x = accordionItems[i];
+      if (index === i) {
+        highlightItem(x);
+        continue;
+      }
+      x.nextElementSibling.classList.remove("accordion_show");
+      x.firstElementChild.nextElementSibling.classList.remove("arrow_rotate");
+      resetItem(x);
+    }
 
-        if(item.nextElementSibling.offsetWidth > 0 && item.nextElementSibling.offsetHeight > 0 === true){
-            item.style.borderBottom = '0px solid rgba(128, 128, 128, 0.445)'; 
-            item.firstElementChild.style.color = 'hsl(14, 88%, 65%)';
-            
-        } else {
-            item.style.borderBottom = '0.1px solid rgba(128, 128, 128, 0.445)'; 
-            item.firstElementChild.style.color = 'hsl(238, 29%, 16%)';
-        }
-        
-        
-    })
+    if (item.nextElementSibling.classList.contains("accordion_show")) {
+      highlightItem(item);
+    } else {
+      resetItem(item);
+    }
+  });
+});
 
-})
+const highlightItem = (item) => {
+  item.style.borderBottom = "0px solid rgba(128, 128, 128, 0.445)";
+  item.firstElementChild.style.color = "hsl(14, 88%, 65%)";
+};
+
+const resetItem = (item) => {
+  item.style.borderBottom = "0.1px solid rgba(128, 128, 128, 0.445)";
+  item.firstElementChild.style.color = "hsl(238, 29%, 16%)";
+};
+
